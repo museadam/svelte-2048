02131dbdaccc5a1134bd45dcaad5dc6ff1916f2c
adam270 <musedadam95@gmail.com>
2024-12-03 16:51:30 -0700

    added a color for each teir

--- src/lib/game/game.svelte
+++ src/lib/game/game.svelte
@@ -28,13 +28,39 @@
 				break;
 		}
 	}
+	const tileColors = {
+		2: '#eee4da',
+		4: '#ede0c8',
+		8: '#f2b179',
+		16: '#f59563',
+		32: '#f67c5f',
+		64: '#f65e3b',
+		128: '#edcf72',
+		256: '#edcc61',
+		512: '#edc850',
+		1024: '#edc53f',
+		2048: '#edc22e'
+	};
+	function getTileColor(value) {
+		if (value in tileColors) {
+			return tileColors[value];
+		} else {
+			// For higher values, you can calculate a color based on the value
+			const hue = 200 - Math.log2(value) * 10;
+			const lightness = 50 - Math.log2(value) * 5;
+			return `hsl(${hue}, 85%, ${lightness}%)`;
+		}
+	}
 </script>
 
 <div class="game-board">
 	{#each game.grid as row}
 		<div class="row">
 			{#each row as tile}
-				<div class="tile {tile > 0 ? 'filled value-' + tile : ''}" style:background-color="">
+				<div
+					class="tile {tile > 0 ? 'filled value-' + tile : ''}"
+					style={tile > 0 ? `background-color: ${getTileColor(tile)}` : ''}
+				>
 					{tile > 0 ? tile : ''}
 				</div>
 			{/each}
@@ -64,12 +90,11 @@
 		font-size: 24px;
 		font-weight: bold;
 		color: #fff;
-		background-color: #eee;
 	}
 
-	.tile.filled {
+	/* .tile.filled {
 		background-color: #776e65;
-	}
+	} */
 
 	/* Add more styles for different tile values */
 	.tile.value-2 {
@@ -77,5 +102,8 @@
 	}
 	.tile.value-4 {
 		background-color: #ede0c8;
+	}
+	.tile.value-8 {
+		background-color: #d7c094;
 	}
 </style>
--- src/lib/game/game.svelte.ts
+++ src/lib/game/game.svelte.ts
@@ -37,9 +37,9 @@ export class Game {
         }
     }
 
-    // ... other game logic methods like moveUp, moveDown, moveLeft, moveRight
 
     moveUp() {
+        let moved = false
         for (let col = 0; col < 4; col++) {
             for (let row = 1; row < 4; row++) {
                 let currentRow = row;
@@ -47,6 +47,8 @@ export class Game {
                     this.grid[currentRow - 1][col] = this.grid[currentRow][col];
                     this.grid[currentRow][col] = 0;
                     currentRow--;
+                    moved = true
+
                 }
 
                 if (currentRow > 0 && this.grid[currentRow - 1][col] === this.grid[currentRow][col]) {
@@ -57,11 +59,13 @@ export class Game {
 
             }
         }
-        this.addRandomTile();
+        if (moved) this.addRandomTile();
 
     }
 
     moveDown() {
+        let moved = false
+
         for (let col = 0; col < 4; col++) {
             for (let row = 2; row >= 0; row--) {
                 let currentRow = row;
@@ -69,21 +73,27 @@ export class Game {
                     this.grid[currentRow + 1][col] = this.grid[currentRow][col];
                     this.grid[currentRow][col] = 0;
                     currentRow++;
+                    moved = true
+
                 }
 
                 if (currentRow < 3 && this.grid[currentRow + 1][col] === this.grid[currentRow][col]) {
                     this.grid[currentRow + 1][col] *= 2;
                     this.grid[currentRow][col] = 0;
                     this.score += this.grid[currentRow + 1][col];
-                    console.log(this.grid)
+                    // console.log(this.grid)
+
                 }
             }
         }
-        this.addRandomTile();
+        if (moved) this.addRandomTile();
+
 
     }
 
     moveLeft() {
+        let moved = false
+
         for (let row = 0; row < 4; row++) {
             for (let col = 1; col < 4; col++) {
                 let currentCol = col;
@@ -91,20 +101,26 @@ export class Game {
                     this.grid[row][currentCol - 1] = this.grid[row][currentCol];
                     this.grid[row][currentCol] = 0;
                     currentCol--;
+                    moved = true
+
                 }
 
                 if (currentCol > 0 && this.grid[row][currentCol - 1] === this.grid[row][currentCol]) {
                     this.grid[row][currentCol - 1] *= 2;
                     this.grid[row][currentCol] = 0;
                     this.score += this.grid[row][currentCol - 1];
+
                 }
             }
         }
-        this.addRandomTile();
+        if (moved) this.addRandomTile();
+
 
     }
 
     moveRight() {
+        let moved = false
+
         for (let row = 0; row < 4; row++) {
             for (let col = 2; col >= 0; col--) {
                 let currentCol = col;
@@ -112,16 +128,20 @@ export class Game {
                     this.grid[row][currentCol + 1] = this.grid[row][currentCol];
                     this.grid[row][currentCol] = 0;
                     currentCol++;
+                    moved = true
+
                 }
 
                 if (currentCol < 3 && this.grid[row][currentCol + 1] === this.grid[row][currentCol]) {
                     this.grid[row][currentCol + 1] *= 2;
                     this.grid[row][currentCol] = 0;
                     this.score += this.grid[row][currentCol + 1];
+
                 }
             }
         }
-        this.addRandomTile();
+        if (moved) this.addRandomTile();
+
 
     }
 
